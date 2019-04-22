59224fe98b390f0012bc91c442fc82ace4b796b4
Benjamin Schüpbach <auteblauwau@hotmail.com>
2019-04-22 16:02:15 +0200

    Slider Problem

--- buffer.js
+++ buffer.js
@@ -1,3 +1,4 @@
 // get value from BufferSlider
-var buff = document.getElementById('BufferSliderOutput');
-console.log(buff)
+// visit https://mdbootstrap.com/docs/jquery/forms/slider/ for more insights.
+var buff = document.getElementById("#BufferSliderOutput").getValue;
+console.log("thats whats logged", buff)
